71a317d65ddd64c8b54a3e7a856dd7c04dffad38
DeadMan-Moc <mokgohloallie@gmail.com>
2019-09-26 12:10:11 +0300

    update

--- src/dice.js
+++ src/dice.js
@@ -3,8 +3,23 @@ class Die {
     this.sides = sides;
     this.probabilities = probabilities;
     sides > 1;
+
+    let probabilities = [];
+    let sum = 0;
+
+    for (let i = 0; i < length.probabilities; i += 1) {
+      if (probabilities[i] < 0) {
+        throw new Error("Negatives not allowed")
+      }
+      if (sum += probabilities[i] < 1){
+        throw new Error("Only integer values allowed")
+      }
+    }
   }
   roll(value){
     this.value = value;
+  }
+  setProbabilities(){
+    
   }
 }
