b3a624aca4837ca8a27b4b8039382545ebf63fc4
DeadMan-Moc <mokgohloallie@gmail.com>
2019-09-27 16:12:04 +0300

    not really done

--- spec/diceSpec.js
+++ spec/diceSpec.js
@@ -1,5 +1,17 @@
-describe("Person", () => {
-    it("should show person's details", () => {
+describe("Dice", () => {
+    it("Should return range of 1 - 6", () => {
+        let die = new Die ();
+        let pop = die.roll(6);
+        expect(pop).toBe()
+    });
+
+    it("Should return range of error message", () => {
+        let die = new Die ();
+        let pop = die.roll();
+        expect(pop).toBe()
+    });
+
+    it("Should return range of 1 - 6", () => {
         let die = new Die ();
         let pop = die.roll();
         expect(pop).toBe()
