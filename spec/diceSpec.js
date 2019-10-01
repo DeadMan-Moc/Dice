33fd6ff195d67efc0d9968c5283096a78ed28eb8
DeadMan-MOC <mokgohloallie@gmail.com>
2019-10-01 10:19:45 +0200

    --- Auto-Git Commit ---

--- spec/diceSpec.js
+++ spec/diceSpec.js
@@ -1,16 +1,17 @@
 describe("Dice should return error messages", () => {
     it("Returns error messages", ()=>{
+        let dice = new Die([])
         
     })    
 })
 
 describe('Dice returns 1 value of its sides', ()=>{
     it('Returns value between 1 and 6', ()=>{
-    let dice = new Die(6)
-    let value = dice.roll()
-    if (value >= 1 && value <= 6) {
-         var range = true;
-    }
-    expect(range).toBe(true);
+        let dice = new Die(6)
+        let value = dice.roll()
+        if (value >= 1 && value <= 6) {
+            var range = true;
+        }
+        expect(range).toBe(true);
     });
 })
